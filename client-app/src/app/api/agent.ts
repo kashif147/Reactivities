d1a7e52c66ab8b041bc599496fd166d7a1c41333
Kashif <kashif@valentiatech.com>
2020-12-29 12:09:16 +0000

    end of section 9

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -7,12 +7,12 @@ const responseBody = (response: AxiosResponse) => response.data;
 
 const sleep = (ms: number) => (response: AxiosResponse) =>
     new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms))
-
+const delay = 0;
 const requests = { 
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
+    get: (url: string) => axios.get(url).then(sleep(delay)).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(sleep(delay)).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(sleep(delay)).then(responseBody),
+    del: (url: string) => axios.delete(url).then(sleep(delay)).then(responseBody)
 }
 
 const Activities ={
